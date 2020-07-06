812c5f58def75eaf0201e0b54a501ed33e66d386
twofist <twofist12345@gmail.com>
2020-07-07 01:52:21 +0200

    fix path for raspberry pi autostart

--- serverNode.mjs
+++ serverNode.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import { exec } from "child_process";
-import path from 'path';
-const __dirname = path.resolve();
+//import path from 'path';
+//const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
@@ -13,18 +13,19 @@ app.get("/", async (req, res) => {
 app.post("/on", async (req, res) => {
     const seconds = parseInt(req.query.seconds) || 1;
     console.log(seconds)
-    await exec('sudo python gpio/on.py');
+    await exec('sudo python /home/pi/projects/relay/gpio/on.py');
     setTimeout(async () => {
-        await exec('sudo python gpio/off.py');
+        await exec('sudo python /home/pi/projects/relay/gpio/off.py');
     }, seconds * 1000);
 })
 
 app.post("/off", async (req, res) => {
-    await exec('sudo python gpio/off.py');
+    await exec('sudo python /home/pi/projects/relay/gpio/off.py');
 })
 
 app.get("/home", async (req, res) => {
-    await res.sendFile(__dirname + "/public/home.html");
+    //await res.sendFile(__dirname + "/public/home.html");
+    await res.sendFile("/home/pi/projects/relay/public/home.html");
 });
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
